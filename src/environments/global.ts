6e272d1b07f9ee139bedae08a009a20d375ebeab
mauriciokwitko <mauricio.kwitko@ezoom.com.br>
2022-11-25 20:12:25 -0300

    global ts

--- src/environments/global.ts
+++ src/environments/global.ts
@@ -1,3 +1,4 @@
+import { fire } from './firebase';
 // TODO alterar 'app' e 'firebase'
 export const global = {
   version: '0.0.2',
@@ -8,16 +9,7 @@ export const global = {
     base: 'https://onesignal.com/api/v1/notifications',
     apiKey: 'Basic ZDU4OWQzYTEtZWY5OC00YTg2LTkzZTgtMjM5MDk0MzU2NDk0',
   },
-  firebase: {
-    projectId: 'clube-template',
-    appId: '1:605097301870:web:866835f13a209b436a937f',
-    storageBucket: 'clube-template.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyCUp2EoiIKWvNJ7ZnNHng3x16ItjmuupcQ',
-    authDomain: 'clube-template.firebaseapp.com',
-    messagingSenderId: '605097301870',
-    measurementId: 'G-5B54SN0DHR',
-  },
+  firebase: fire.firebase,
   paths: {
     users: {
       myUser: 'myUser',
